6ea8994ac18895ea229e3f6c23be0608f025178a
arvis <viestards.lists@gmail.com>
2013-07-30 00:02:07 +0300

    added Test Suite

---
+++ src/test/java/com/viestards/formhelper/FieldItemsSuite.java
@@ -0,0 +1,12 @@
+package com.viestards.formhelper;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ComboBoxFieldTest.class, FormHelperTest.class,
+		InputFieldTest.class, OptionFieldTest.class, TextAreaFieldTest.class })
+public class FieldItemsSuite {
+
+}
